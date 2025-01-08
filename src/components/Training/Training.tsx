f0912edc59ae9397430e980e310085d44c610f6f
Anteneh Mulu <anteneh.mulu1921@gmail.com>
2025-01-08 16:22:09 +0300

    add training section

--- src/components/Training/Training.tsx
+++ src/components/Training/Training.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Training = () => {
   const photos: string[] = [
